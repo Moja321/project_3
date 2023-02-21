3444543fd7f5463c627b32aab7a4bc481e9cf553
fiz232 <fionalim1997@hotmail.com>
2023-02-21 09:52:20 +0800

    updated origami schemas

--- Backend/models/origami_schema.js
+++ Backend/models/origami_schema.js
@@ -1,9 +1,14 @@
 const mongoose = require("mongoose");
 
 const origamiSchema = new mongoose.Schema({
+  posterid: { type: String },
   name: { type: String, required: true },
+  likes: { type: Number },
+  title: { type: String, required: true },
   img: { type: String },
   description: { type: String },
+  reference: { type: String },
+  instructions: { type: String },
 });
 
 const origami = mongoose.model("origami", origamiSchema);
